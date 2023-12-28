2dcf0387e292377b0a99b2c225695e5ddd4e5ab1
James Rissler <risslerjames@gmail.com>
2023-12-28 11:05:48 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -32,11 +32,11 @@ defmodule ChartsLive.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.28.5", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.15.0", only: :test},
-      {:phoenix_live_view, "~> 0.18.2"},
+      {:credo, "~> 1.7.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.2", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.0", only: :test},
+      {:phoenix_live_view, "~> 0.20.2"},
       {:number, "~> 1.0.4"},
       {:charts, github: "jrissler/charts"}
       # {:charts, path: "../charts"}
